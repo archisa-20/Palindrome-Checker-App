750b72481e58190490b55a7f144292f98ff68f22
archisa-20 <archisadas20@gmail.com>
2026-02-27 14:28:19 +0530

    UC1: Add welcome message module

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,2 +1,11 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args){
+
+        // ================= UC1: Application Entry and Welcome Message =================
+
+        System.out.println("======================================");
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+    }
 }
