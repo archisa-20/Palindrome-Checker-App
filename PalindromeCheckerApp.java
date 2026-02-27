8174ee5e49ab425344a5124cf1cd9764fae9c64b
archisa-20 <archisadas20@gmail.com>
2026-02-27 15:03:58 +0530

    UC2:Hardcoded palindrome

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,11 +1,25 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
 
-        // ================= UC1: Application Entry and Welcome Message =================
+    // ================= UC1: Application Entry and Welcome Message =================
 
         System.out.println("======================================");
         System.out.println("Welcome to Palindrome Checker Management System");
         System.out.println("Version: 1.0");
         System.out.println("System initialized successfully");
+
+    // ================= UC2: Hardcoded Palindrome Validation =================
+        String word = "madam";
+        String reversed="";
+        System.out.println("Input Text: madam");
+
+        for (int i = word.length() - 1; i >= 0; i--){
+            reversed += word.charAt(i);
+        }
+        if (word.equals(reversed)) {
+            System.out.println(word + " is Palindrome");
+        }else {
+            System.out.println(word + " is NOT Palindrome");
+        }
     }
 }
